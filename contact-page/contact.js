6a3079e8475f44e3e5e66ca4bd6d7eea5e27163c
Tinotenda-K <karambakuwatinotenda@gmail.com>
2022-07-23 12:11:07 +0100

    Basic structure and properties of all 3 pages complted: Home, About and Contact page

---
+++ contact-page/contact.js
@@ -0,0 +1,10 @@
+import LocomotiveScroll from '/node_modules/locomotive-scroll/dist/locomotive-scroll.esm.js';
+// import LocomotiveScroll from "https://cdn.skypack.dev/locomotive-scroll";
+
+const scroller = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    lerp: 0.5,
+    multiplier: 0.9
+    // touchMultiplier: 0.5
+  });
